ed46de0ba04cfa602afffc29355d31f987de39a2
Lakshay Saini <44637081+Saini-Lakshay@users.noreply.github.com>
2019-08-05 14:10:53 +0530

    Flip bit using xor and left shift

---
+++ flip_bit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main()
+{
+	
+	printf("Enter the number : ");
+	int n;
+	scanf("%d",&n);
+	
+	printf("Enter the bit you want to change : ");
+	int i;
+	scanf("%d",&i);
+	
+	int ans=(n^(1<<(i-1)));
+	
+	printf("New number is : %d",ans);
+	
+	return 0;
+}
